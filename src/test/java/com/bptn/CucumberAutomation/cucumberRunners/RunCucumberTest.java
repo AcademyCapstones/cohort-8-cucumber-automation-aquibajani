d6d73ac8934cc7b6b60a1d9d6e2f11aa8a3559d1
aquibajani <aquib.ajani@obsidi.com>
2025-12-04 13:29:39 -0500

    Coding 4.8: Shopping Cart Scenario Feature

--- src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
+++ src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
@@ -6,17 +6,17 @@ import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-//@SelectClasspathResource("com/bptn/CucumberAutomation/features/example.feature")
-//@SelectClasspathResource("com/bptn/CucumberAutomation/features/calculator.feature")
-//@SelectClasspathResource("com/bptn/CucumberAutomation/features/strongPassword.feature")
-@SelectClasspathResource("com/bptn/CucumberAutomation/signUp.feature")
-//@SelectClasspathResource("com/bptn/CucumberAutomation/features/login.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/example.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/calculator.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/strongPassword.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/signUp.feature")
+//@SelectClasspathResource("com/bptn/CucumberAutomation/login.feature")
+@SelectClasspathResource("com/bptn/CucumberAutomation/shoppingCart.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bptn.CucumberAutomation")
-@IncludeTags("ValidUserInformation")
+//@IncludeTags("ValidUserInformation")
 public class RunCucumberTest {
 }
